0974662ac3c84c7e2446839ecaf4d73e7ddc616f
桑山祐一 <kuwayamayuuichi@kuwayamayuuichinoiMac.local>
2017-09-16 16:34:50 +0900

    ある程度

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,9 +1,147 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
 
-$(document).ready(function () {
-    hsize = $(window).height();
-    $("#s01").css("height", hsize + "px");
+//= require twitter/bootstrap
+//= require bootstrap-material-design
+//= require turbolinks
+//= require_tree .
+
+
+// $(document).ready(function () {
+//     hsize = $(window).height();
+//     $("#first").css("height", hsize + "px");
+// });
+// $(window).resize(function () {
+//     hsize = $(window).height();
+//     $("#first").css("height", hsize + "px");
+// });
+
+$(function () {
+    $('#massage').textillate({
+      //繰り返し
+      loop: false,
+      // アニメーションの間隔時間
+      minDisplayTime: 3000,
+      // アニメーション開始までの遅延時間
+      initialDelay: 1000,
+      // アニメーションの自動スタート
+      autoStart: true,
+
+      // 開始時のアニメーション設定
+      in: {
+        // エフェクトの指定
+        effect: 'fadeIn',
+        // 遅延時間の指数
+        delayScale: 1.5,
+        // 文字ごとの遅延時間
+        delay: 50,
+        // true:アニメーションをすべての文字に同時適用
+        sync: false,
+        // true:文字表示がランダムな順に表示される
+        shuffle: false
+      },
+
+      // 終了時のアニメーション設定
+      // out: {
+      //   effect: 'fadeOut',
+      //   delayScale: 1.5,
+      //   delay: 50,
+      //   sync: false,
+      //   shuffle: false
+      // }
+    });
+})
+$(function () {
+    $('.massage2').textillate({
+      //繰り返し
+      loop: false,
+      // アニメーションの間隔時間
+      minDisplayTime: 3000,
+      // アニメーション開始までの遅延時間
+      initialDelay: 1000,
+      // アニメーションの自動スタート
+      autoStart: true,
+
+      // 開始時のアニメーション設定
+      in: {
+        // エフェクトの指定
+        effect: 'fadeIn',
+        // 遅延時間の指数
+        delayScale: 1.5,
+        // 文字ごとの遅延時間
+        delay: 50,
+        // true:アニメーションをすべての文字に同時適用
+        sync: false,
+        // true:文字表示がランダムな順に表示される
+        shuffle: false
+      },
+
+      // 終了時のアニメーション設定
+      // out: {
+      //   effect: 'fadeOut',
+      //   delayScale: 1.5,
+      //   delay: 50,
+      //   sync: false,
+      //   shuffle: false
+      // }
+    });
+})
+
+
+
+$(function() {
+    var bgsc1 = 0;
+    var bgsc2 = 0;
+setInterval(function(){
+    bgsc1+= 0.1;//マイナスは左に、プラスは右に移動します。この場合だと-1なので左に1pxづつ移動します。右に移動させる場合は1に変えます。
+    bgsc2+= 0;//マイナスは上に、プラスは下に移動。このサンプルは0にしてあるので上下には移動しません。
+    $(".skinBody").css("background-position", bgsc1 + "px " + bgsc2 + "px");
 });
-$(window).resize(function () {
-    hsize = $(window).height();
-    $("#s01").css("height", hsize + "px");
 });
+
+
+
+
+
+
+
+$(function() {
+        // フェードイン
+        $('.inviewfadeIn').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+            if (isInView) {
+                $(this).stop().addClass('fadeIn');
+            } else {
+                $(this).stop().removeClass('fadeIn');
+            }
+        });
+    });
+
+ $(function(){
+    $('.slides').slick({
+      fade: true,
+      arrows: false,
+      asNavFor: '.slides-nav'
+    });
+    $('.slides-nav').slick({
+      dots: false,
+      arrows: false,
+      slidesToShow: 5,
+      centerMode: true,
+      focusOnSelect: true,
+      asNavFor: '.slides',
+    });
+  });
+
+
